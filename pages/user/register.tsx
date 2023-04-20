4758c9caacb9b1a69fc20101f8390864bac1d8f6
AdrianMonenciu <101457701+AdrianMonenciu@users.noreply.github.com>
2023-04-20 21:27:46 +0100

    Update 2 2023-04-20

--- pages/user/register.tsx
+++ pages/user/register.tsx
@@ -143,37 +143,41 @@ export default function Register(){
 
                 {/* form */}
                 <form className='flex flex-col gap-5 items-center' onSubmit={formik.handleSubmit}>
-                    <div className={`${styles.input_group} ${formik.errors.username && formik.touched.username ? 'border-rose-600' : ''}`}>
-                        <input 
-                        type="text"
-                        name='Username'
-                        placeholder='Username'
-                        className={styles.input_text}
-                        {...formik.getFieldProps('username')}
-                        />
-                        <span className='icon flex items-center px-4'>
-                            <HiOutlineUser size={25} />
-                        </span>
+                    <div className='flex flex-col'>
+                        <div className={`${styles.input_group} ${formik.errors.username && formik.touched.username ? 'border-rose-600' : ''}`}>
+                            <input 
+                            type="text"
+                            name='Username'
+                            placeholder='Username'
+                            className={styles.input_text}
+                            {...formik.getFieldProps('username')}
+                            />
+                            <span className='icon flex items-center px-4'>
+                                <HiOutlineUser size={25} />
+                            </span>
+                        </div>
+                        {formik.errors.username && formik.touched.username ? 
+                        <span className={`${styles.formikError}`}>{formik.errors.username}</span> : <></>}
+                        {/* {formik.errors.username && formik.touched.username ? <span className='text-rose-500'>{formik.errors.username}</span> : <></>} */}
                     </div>
-                    
-                    {formik.errors.username && formik.touched.username ? <span className='text-rose-500'>{formik.errors.username}</span> : <></>}
-                    {/* {formik.errors.username && formik.touched.username ? <span className='text-rose-500'>{formik.errors.username}</span> : <></>} */}
-                    
-                    <div className={`${styles.input_group} ${formik.errors.email && formik.touched.email ? 'border-rose-600' : ''}`}>
-                        <input 
-                        type="email"
-                        name='email'
-                        placeholder='Email'
-                        className={styles.input_text}
-                        {...formik.getFieldProps('email')}
-                        />
-                        <span className='icon flex items-center px-4'>
-                            <HiAtSymbol size={25} />
-                        </span>
+
+                    <div className='flex flex-col'>
+                        <div className={`${styles.input_group} ${formik.errors.email && formik.touched.email ? 'border-rose-600' : ''}`}>
+                            <input 
+                            type="email"
+                            name='email'
+                            placeholder='Email'
+                            className={styles.input_text}
+                            {...formik.getFieldProps('email')}
+                            />
+                            <span className='icon flex items-center px-4'>
+                                <HiAtSymbol size={25} />
+                            </span>
+                        </div>
+                        {formik.errors.email && formik.touched.email ? 
+                        <span className={`${styles.formikError}`}>{formik.errors.email}</span> : <></>}
+                        {/* {formik.errors.email && formik.touched.email ? <span className='text-rose-500'>{formik.errors.email}</span> : <></>} */}
                     </div>
-                    {formik.errors.email && formik.touched.email ? <span className='text-rose-500'>{formik.errors.email}</span> : <></>}
-                    {/* {formik.errors.email && formik.touched.email ? <span className='text-rose-500'>{formik.errors.email}</span> : <></>} */}
-                    
 
                     {/*<div className={`${styles.input_group} ${formik.values.dietPreference == '' && formik.touched.username ? 'border-rose-600' : ''}`}>
                         <select name="dietPreference" value={formik.values.dietPreference} className={styles.input_text} {...formik.getFieldProps('dietPreference')}>
@@ -220,42 +224,49 @@ export default function Register(){
                             {diet} </label>)}
                         </div>
 
-                        {formik.errors.dietPreference && formik.touched.dietPreference && !formik.values.noDiet ? <span className='text-rose-500 mb-1 ml-1'>{formik.errors.dietPreference}</span> : <></>}
+                        {formik.errors.dietPreference && formik.touched.dietPreference && !formik.values.noDiet ? 
+                        <span className='text-rose-500 mb-1 ml-1 text-sm md:text-base'>{formik.errors.dietPreference}</span> : <></>}
                     </div>
 
 
-                    <div className={`${styles.input_group} ${formik.errors.password && formik.touched.password ? 'border-rose-600' : ''}`}>
-                        <input 
-                        type={`${show.password ? "text" : "password"}`}
-                        name='password'
-                        placeholder='Password'
-                        className={styles.input_text}
-                        {...formik.getFieldProps('password')}
-                        />
-                        <span className='icon flex items-center px-4' onClick={() => setShow({ ...show, password: !show.password})}>
-                            <HiFingerPrint size={25} />
-                        </span>
+                    <div className='flex flex-col'>
+                        <div className={`${styles.input_group} ${formik.errors.password && formik.touched.password ? 'border-rose-600' : ''}`}>
+                            <input 
+                            type={`${show.password ? "text" : "password"}`}
+                            name='password'
+                            placeholder='Password'
+                            className={styles.input_text}
+                            {...formik.getFieldProps('password')}
+                            />
+                            <span className='icon flex items-center px-4' onClick={() => setShow({ ...show, password: !show.password})}>
+                                <HiFingerPrint size={25} />
+                            </span>
+                        </div>
+                        {formik.errors.password && formik.touched.password ? 
+                        <span className={`${styles.formikError}`}>{formik.errors.password}</span> : <></>}
+                        {/* {formik.errors.password && formik.touched.password ? <span className='text-rose-500'>{formik.errors.password}</span> : <></>} */}
                     </div>
-                    {formik.errors.password && formik.touched.password ? <span className='text-rose-500'>{formik.errors.password}</span> : <></>}
-                    {/* {formik.errors.password && formik.touched.password ? <span className='text-rose-500'>{formik.errors.password}</span> : <></>} */}
 
-                    <div className={`${styles.input_group} ${formik.errors.cpassword && formik.touched.cpassword ? 'border-rose-600' : ''}`}>
-                        <input 
-                        type={`${show.cpassword ? "text" : "password"}`}
-                        name='cpassword'
-                        placeholder='Confirm Password'
-                        className={styles.input_text}
-                        {...formik.getFieldProps('cpassword')}
-                        />
-                        <span className='icon flex items-center px-4' onClick={() => setShow({ ...show, cpassword: !show.cpassword})}>
-                            <HiFingerPrint size={25} />
-                        </span>
+
+                    <div className='flex flex-col'>
+                        <div className={`${styles.input_group} ${formik.errors.cpassword && formik.touched.cpassword ? 'border-rose-600' : ''}`}>
+                            <input 
+                            type={`${show.cpassword ? "text" : "password"}`}
+                            name='cpassword'
+                            placeholder='Confirm Password'
+                            className={styles.input_text}
+                            {...formik.getFieldProps('cpassword')}
+                            />
+                            <span className='icon flex items-center px-4' onClick={() => setShow({ ...show, cpassword: !show.cpassword})}>
+                                <HiFingerPrint size={25} />
+                            </span>
+                        </div>
+                        {formik.errors.cpassword && formik.touched.cpassword ? 
+                        <span className={`${styles.formikError}`}>{formik.errors.cpassword}</span> : <></>}
+                        {/* {formik.errors.cpassword && formik.touched.cpassword ? <span className='text-rose-500'>{formik.errors.cpassword}</span> : <></>} */}
                     </div>
-                    {formik.errors.cpassword && formik.touched.cpassword ? <span className='text-rose-500'>{formik.errors.cpassword}</span> : <></>}
-                    {/* {formik.errors.cpassword && formik.touched.cpassword ? <span className='text-rose-500'>{formik.errors.cpassword}</span> : <></>} */}
 
-
-                    <div className='flex'>
+                    <div className='flex flex-col'>
                         <label htmlFor="image" className={`${styles.button} min-w-[150px] max-w-[200px] text-center`}>
                             Choose File
                         </label>
@@ -279,9 +290,10 @@ export default function Register(){
                         }}
                         className={'hidden'}
                         ></input>
-                    </div>
 
-                    {formik.errors.image && formik.touched.image ? <span className='text-rose-500'>{formik.errors.image}</span> : <></>}
+                        {formik.errors.image && formik.touched.image ? 
+                        <span className='text-rose-500 mt-1 ml-1 text-sm md:text-base'>{formik.errors.image}</span> : <></>}
+                    </div>
 
                     <div>
                         {formik.values.image ?
